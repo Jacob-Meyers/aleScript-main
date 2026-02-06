63865023cd63a773d2d2b9a8d5c04f354fe13ddb
Jacob Meyers <jakejem2010@gmail.com>
2026-02-06 17:15:14 -0500

    v1.3-n2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "interpreter.h"
 
 int main(int argc, char* argv[]) {
-    string ale_version = "v1.3-n1";
+    string ale_version = "v1.3-n2";
 
     if (argc < 2) {
         std::cerr << "Alescript " << ale_version << "\nUsage: interpreter <program_file>\n";
